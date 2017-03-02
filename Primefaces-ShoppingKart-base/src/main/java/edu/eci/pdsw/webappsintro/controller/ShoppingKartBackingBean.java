79445844a82ce16bfac0358c7141c2ad73344161
kvncf <kevinju92@outlook.com>
2017-03-02 00:13:10 -0500

    terminado

--- Primefaces-ShoppingKart-base/src/main/java/edu/eci/pdsw/webappsintro/controller/ShoppingKartBackingBean.java
+++ Primefaces-ShoppingKart-base/src/main/java/edu/eci/pdsw/webappsintro/controller/ShoppingKartBackingBean.java
@@ -35,7 +35,41 @@ import javax.faces.bean.SessionScoped;
 public class ShoppingKartBackingBean implements Serializable{
     private ArrayList<int[]> pedido=new ArrayList<int[]>();
     private Producto itemSelect = null;
-    private String itemName="-X--";  
+    private String itemName="Para empezar selecciona un item";
+    private int cantActual=0; 
+    private ArrayList<String[]> ListaPedido;
+
+    public ArrayList<String[]> getListaPedido() {
+        ListaPedido=new ArrayList<String[]>();
+        for(int i=0;i<pedido.size();i++){
+            /*
+            int[] p={idProd,cant};
+            pedido.add(p);
+            */
+            if(pedido.get(i)[1]!=0){
+                String[] tmp={nombreID(pedido.get(i)[0]),Integer.toString(pedido.get(i)[1])};
+                ListaPedido.add(tmp);
+            }
+        }
+        return ListaPedido;
+    }
+    
+    
+    private String nombreID(int id){
+        List<Producto> p=getProductos();
+        String res="";
+        for(int i=0;i<p.size();i++){
+            if(p.get(i).getId()==id){
+                res=p.get(i).getNombre();
+                break;
+            }
+        }
+        return res;
+    }
+
+    public void setListaPedido(ArrayList<String[]> ListaPedido) {
+        this.ListaPedido = ListaPedido;
+    }
 
     public String getItemName() {
         return itemName;
@@ -71,13 +105,44 @@ public class ShoppingKartBackingBean implements Serializable{
         
     }
     
+    public void agregar(){
+        int id=itemSelect.getId();
+        cantActual = Math.max(0,cantActual);
+        boolean yaExiste=false;
+        for(int i=0; i<pedido.size();i++){
+            if(pedido.get(i)[0]==id){
+                pedido.get(i)[1]=cantActual;
+                yaExiste=true;
+                break;
+            }
+        }
+        if(!yaExiste){//si no habia sido agregado el producto
+            int[] p={id,cantActual};
+            pedido.add(p);
+        }   
+        
+    }
+
+    public int getCantActual() {
+        return cantActual;
+    }
+
+    public void setCantActual(int cantActual) {
+        this.cantActual = cantActual;
+    }
+    
+    private int updateCantidad(){
+        int id=itemSelect.getId();
+        cantActual=0;
+        for(int i=0;i<pedido.size();i++){
+            if(pedido.get(i)[0]==id)cantActual = pedido.get(i)[1];        
+        }         
+        return cantActual;
+    }
     
     public void seleccionarItem(Producto prod){
         itemName=prod.getNombre();
         itemSelect=prod;
-    }
-    
-    public void reiniciar(){
-        itemName=":(";
+        updateCantidad();
     }
 }
