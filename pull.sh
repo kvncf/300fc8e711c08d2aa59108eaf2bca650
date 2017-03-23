97d9aebd4ae57d7d2d749e706f62a077c92f46ca
dark07perez <dark07perez@gmail.com>
2017-03-23 11:05:17 -0500

    archivos sh lab6.3

---
+++ pull.sh
@@ -0,0 +1,5 @@
+echo Debio haber commitado primero!!!  comite, add .., commit -m
+git pull --rebase origin 
+git pull origin lab6.3
+git push origin lab6.3
+echo Pull, Pull --rebase y push Finalizado!!!
---
+++ push.sh
@@ -0,0 +1,3 @@
+echo Debio haber commitado primero!!!  comite, add .., commit -m
+git push origin lab6.3
+echo push Finalizado!!!
